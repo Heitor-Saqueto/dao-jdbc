aa9d84bbf43a724eaef6e9b202ce364a2dfa1fa6
Heitor-Saqueto <heitorsaqueto@hotmail.com>
2025-01-01 13:13:36 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
+		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(01, "Fulano", "funalo@detao.com",new Date(),8000.0, obj);
+		System.out.println(seller);
 	}
 
 }
