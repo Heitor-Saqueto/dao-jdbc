c162291b3ae23f1b371422f327b81dae62d6787c
Heitor-Saqueto <heitorsaqueto@hotmail.com>
2025-01-01 21:59:26 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,9 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +16,9 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		
 		Seller seller = new Seller(01, "Fulano", "funalo@detao.com",new Date(),8000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(seller);
 	}
 
