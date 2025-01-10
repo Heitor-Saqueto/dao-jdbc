b7e74975d0eed52ef86f85610726952c36e81382
Heitor-Saqueto <heitorsaqueto@hotmail.com>
2025-01-09 23:10:54 -0300

    Delete byId

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.Scanner;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -7,12 +9,22 @@ import model.entities.Department;
 public class Program2 {
 
 	public static void main(String[] args) {
-
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("=== TEST 1 department findById =====");
 		Department department = departmentDao.findById(4);
 		System.out.println(department);
+		
+		System.out.println("=== TEST 2 department deleteById =====");
+		System.out.println("Delete department by id: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
 	}
 
+	
 }
