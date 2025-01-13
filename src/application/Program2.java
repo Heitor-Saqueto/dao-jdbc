5d55a2aac1cb093dc1a6db1af6dc2d66ed565e97
Heitor-Saqueto <heitorsaqueto@hotmail.com>
2025-01-12 22:58:11 -0300

    department update and findAll implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
@@ -17,17 +18,28 @@ public class Program2 {
 		Department department = departmentDao.findById(4);
 		System.out.println(department);
 		
-//		System.out.println("=== TEST 2 department deleteById =====");
-//		System.out.println("Delete department by id: ");
-//		int id = sc.nextInt();
-//		departmentDao.deleteById(id);
-//		System.out.println("Delete completed");
+		System.out.println("=== TEST 2 department deleteById =====");
+		System.out.println("Delete department by id: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
 		
 		System.out.println("=== TEST 3 department insert =====");
-		department = new Department(null, "Robots");
+		department = new Department(null, "New Robots");
 		departmentDao.insert(department);
 		System.out.println("New department inserted with id:"+department.getId());
 		
+		System.out.println("=== TEST 4 department update =====");
+		department = departmentDao.findById(1);
+		department.setName("TOME");
+		departmentDao.update(department);
+		System.out.println("Update completed");
+		
+		System.out.println("\n=== TEST 5 department findAll =====");
+		List<Department> list = departmentDao.findAll();
+		for(Department obj : list) {
+			System.out.println(obj);
+		}
 		sc.close();
 	}
 
